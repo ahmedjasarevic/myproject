394c693cab709ae78553fbd9d95d6b0ae3c2d051
ahmedjasarevic <jasarevicahmed999@gmail.com>
2020-06-18 12:08:58 +0200

    First

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QLabel>
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel("My First APP");
+    label->setWindowTitle("Ahmed Jasarevic's first app");
+    label->resize(300,300);
+    label->show();
+    return app.exec ();
+
+
+   }
+
